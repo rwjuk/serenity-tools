5c9cc98129de34bdfca785d166a6fe2c96e618dc
Richard Jenkins <rwj@serenity.uk.net>
2019-06-12 15:30:44 +0100

    add postcode single action

--- static/postcodes.js
+++ static/postcodes.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 	$("#btn-calc-single-postcode").click(function(){
-		alert("foo");
+		var p1 = $("#postcode1").val();
+		var p2 = $("#postcode2").val();
+		if (p1 !== "" && p2 !== "")
+		{
+			
+			$.get( `/api/postcodes/singledistance/${p1}/${p2}`, function( data ) {
+				var data_spl = data.split(",");
+				var outputBox = $("#output");
+				outputBox.val(outputBox.val() + `&#13;&#10;Distance between ${p1} and ${p2}: ${data_spl[0]} miles, ${data_spl[1]} km`);
+			});
+		}
 	});
 });
