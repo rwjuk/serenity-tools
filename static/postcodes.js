52bc1db5837ade1e265d4fe6218ef8d2fc43bac0
Richard Jenkins <rwj@serenity.uk.net>
2019-06-12 16:19:30 +0100

    test multi implementation

--- static/postcodes.js
+++ static/postcodes.js
@@ -4,11 +4,25 @@ $(document).ready(function(){
 		var p2 = $("#postcode2").val().toUpperCase();
 		if (p1 !== "" && p2 !== "")
 		{
-			
 			jQuery.get( `/api/postcodes/singledistance/${p1}/${p2}`, function( data ) {
 				var data_spl = data.split(",");
 				var outputBox = $("#output");
 				outputBox.val(outputBox.val() + `Distance between ${p1} and ${p2}: ${data_spl[0]} miles, ${data_spl[1]} km\r\n`);
+			});
+		}
+	});
+	
+	$("#btn-calc-multi-postcode").click(function(){
+		var ref = $("#ref_postcode").val().toUpperCase();
+		var test_list = $("#postcode2").val().split("\n");
+		if (ref !== "" && text !== "")
+		{
+			jQuery.post( `/api/postcodes/batchdistance/`, { reference_postcode: ref, test_postcode_list: test_list} , function( data ) {
+				/*var data_spl = data.split(",");
+				var outputBox = $("#output");
+				outputBox.val(outputBox.val() + `Distance between ${p1} and ${p2}: ${data_spl[0]} miles, ${data_spl[1]} km\r\n`);*/
+				
+				outputBox.val(outputBox.val() + data);
 			});
 		}
 	});
