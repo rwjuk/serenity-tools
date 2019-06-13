86170ee6805d3a7ef95df07ead6a39f7d8c323f0
Richard Jenkins <rwj@serenity.uk.net>
2019-06-13 11:35:50 +0100

    uwsgi fix: app to application

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify, render_template
-app = Flask(__name__)
+application = Flask(__name__)
 
 import requests
 import json
 
 from geopy.distance import great_circle
 
-@app.route('/about')
+@application.route('/about')
 def render_about():
     return render_template("about.html")
 
-@app.route('/postcodes')
+@application.route('/postcodes')
 def render_postcode_tools():
     return render_template("postcode_tools.html")
 
@@ -20,7 +20,7 @@ def get_postcode_lat_lng(postcode):
     return (float(data["result"]["latitude"]), float(data["result"]["longitude"]))
 
 
-@app.route('/api/postcodes/singledistance/<p1>/<p2>')
+@application.route('/api/postcodes/singledistance/<p1>/<p2>')
 def calculate_postcode_distance(p1, p2):
     latlng_p1 = get_postcode_lat_lng(p1)
     latlng_p2 = get_postcode_lat_lng(p2)
@@ -29,7 +29,7 @@ def calculate_postcode_distance(p1, p2):
     return "{},{}".format(round(distance.miles,3), round(distance.km,3))
 
 
-@app.route('/api/postcodes/batchdistance', methods=['POST'])
+@application.route('/api/postcodes/batchdistance', methods=['POST'])
 def calculate_batch_postcode_distance():
     obj = request.get_json(force=True)
     reference_postcode = obj['reference_postcode']
@@ -55,4 +55,4 @@ def calculate_batch_postcode_distance():
         return jsonify(distance_list)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    application.run(host='0.0.0.0')
